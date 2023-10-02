e839d6559b22ba353c0c946773a1b47492a703ef
Dheeraj Marwaha <dhmarwah@amazon.com>
2023-10-03 00:39:23 +0530

    fixing the datbase connection

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def sub(x, y):
+    """ subtract y from x and return result"""
+    return x-y
